6c30d657228e27e21f01c8026fccdd1cddbf5a95
Samora Dake <samoradake@gmail.com>
2014-04-04 15:28:48 +0000

    upgrade

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Based on JSend specification ([http://labs.omniti.com/labs/jsend](http://labs.om
 ## Installation
 
 ```
-npm install koa-jsend
+npm install --save koa-jsend
 ```
 
 In your [koa](http://koajs.com) app.
+
 ```javascript
 var koa = require('koa')
   , jsend = require('koa-jsend')
@@ -20,17 +21,17 @@ app.use(jsend());
 ```
 
 ## API
-Instead of using `this.body=` to send JSON responses, use one of the functions in `this.jsend`.
 
-### `success`
+Instead of using `this.body=` to send JSON responses, use one of the functions in `this`.
+Where `this` is the koa context, `ctx`.
 
-`jsend.success(data)`
+### `ctx.success(data)`
 
 * __data__: _Optional_ response data.
 
 ```javascript
 app.use(function *(){
-  this.jsend.success({
+  this.success({
     name: 'Samora',
     sex: 'Male',
     nationality: 'Ghanaian'
@@ -48,15 +49,13 @@ app.use(function *(){
 // }
 ```
 
-### `fail`
-
-`jsend.fail(errors)`
+### `ctx.fail(errors)`
 
 * __errors__: _Required_ error(s) data.
 
 ```javascript
 app.use(function *(){
-  this.jsend.fail({
+  this.fail({
     name: 'Name is required.'
   });
 });
@@ -70,23 +69,67 @@ app.use(function *(){
 // }
 ```
 
-### `error`
+### `ctx.error(message, data, code)`
 
-`jsend.error(message, data, code)`
-
-* __message__: _Required_ error message.
+* __message__: _Required_ error message or `Error` object.
 * __data__: _Optional_ error data.
 * __code__: _Optional_ error code.
 
 ```javascript
 app.use(function *(){
-  this.jsend.error('Something went wrong.', null, 8);
+  this.error('Something went wrong.', null, 8);
 });
 
 // JSON response
 // {
-//  "status": "errror",
+//  "status": "error",
 //  "message": "Something went wrong.",
 //  "code": 8
 // }
 ```
+
+### `ctx.jsend(err, data, code)`
+
+Sends a `ctx.success` response if no `err` is passed.
+Sends a `ctx.error` response if `err` is passed.
+
+* __err__: Error object or message string or null.
+* __data__: _Optional_
+* __code__: _Optional_
+
+```javascript
+app.use(function *(){
+  this.jsend(null, {
+    name: 'Samora',
+    sex: 'Male',
+    nationality: 'Ghanaian'
+  });
+});
+
+// JSON response
+// {
+//   "status": "success",
+//   "data": {
+//     "name": "Samora",
+//     "sex": "Male",
+//     "nationality": "Ghanaian"
+//   }
+// }
+```
+
+
+```javascript
+app.use(function *(){
+  this.jsend(Error('Something went wrong.'));
+});
+
+// JSON response
+// {
+//   "status": "error",
+//   "message": "Something went wrong.",
+// }
+```
+
+
+# License
+MIT
--- index.js
+++ index.js
@@ -1,45 +1,81 @@
-// Koa JSend
+// koa JSend
 // Simple structured JSON responses for your API
 // Based on JSend (http://labs.omniti.com/labs/jsend)
 
 module.exports = function (){
   return function *(next){
-
+    // koa context
     var that = this;
-    that.jsend = {
+    
 
-      // @param data: (object/array)
-      success: function (data){
-        that.body = {
-          status: 'success',
-          data: data || null
-        };
-      },
+    /**
+     * Augment koa context to have success response.
+     *
+     * @param {object} data
+     */
+    that.success = function (data){
+      that.body = {
+        status: 'success',
+        data: data || null
+      };
+    };
 
-      // @param errors: (object)
-      fail: function (errors){
-        that.body = {
-          status: 'fail',
-          data: errors
-        };
-      },
 
-      // @param message: (string)
-      // @param data: (object) - optional
-      // @param code: (string/number) - optional
-      error: function (message, data, code){
-        that.body = {
-          status: 'error',
-          message: String(message)
-        };
+    /**
+     * Augment koa context to have `fail` response.
+     *
+     * @param {object} errors
+     */
+    that.fail = function (errors){
+      that.body = {
+        status: 'fail',
+        data: errors
+      };
+    };
 
-        if (data && typeof(data) === 'object') 
-          that.body.data = data;
 
-        if (code && (typeof(code) === 'string' || typeof(code) === 'number')) 
-          that.body.code = code;
-      },
+    /**
+     * Augment koa context to have `error` response.
+     *
+     * @param {string/error} message
+     * @param {object} data optional
+     * @param {string/number} code optional
+     */
+    that.error = function (message, data, code){
 
+      if (!(message instanceof Error) && (typeof message !== 'string'))
+        throw new Error('Invalid message. Must be error object or string.');
+
+      that.body = {
+        status: 'error'
+      };
+
+      if (message instanceof Error)
+        that.body.message = message.message;
+
+      if (typeof message === 'string')
+        that.body.message = message;
+
+      if (data && typeof(data) === 'object') 
+        that.body.data = data;
+
+      if (code && (typeof(code) === 'string' || typeof(code) === 'number')) 
+        that.body.code = code;
+    };
+
+
+    /**
+     * Send `success` or `error` JSend response.
+     *
+     * @param {string/error/null} err
+     * @param {object} data
+     * @param {string/number} code
+     */
+    that.jsend = function (err, data, code){
+      if (err)
+        return that.error(err, data, code);
+
+      that.success(data);
     };
 
     yield next;
